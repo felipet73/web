9e27dcc049fcb0e1e449cf96ed2e8f7a8fc00311
lleroc <llleroc@yahoo.es>
2025-08-07 20:41:30 -0500

    semana2_clase2_fin

---
+++ Semana 02/Tipo_Datos/Controllers/ClientesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tipo_Datos.Data;
+using Tipo_Datos.Models.Entidades;
+
+namespace Tipo_Datos.Controllers
+{
+    public class ClientesController : Controller
+    {
+        private readonly DatosDbContext _dbContext;
+        public ClientesController(DatosDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _dbContext.Clientes.ToListAsync());
+        }
+
+        public IActionResult Nuevo() {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> 
+            Nuevo([Bind("Nombres,Email,Telefono,Direccion,Cedula_RUC," +
+            "Create_At,Update_At,isDelete")] ClientesModel cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Add(cliente);
+                await _dbContext.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return  View(cliente);
+        }
+
+
+    }
+}
+
+
--- Semana 02/Tipo_Datos/obj/Debug/net8.0/Tipo_Datos.AssemblyInfo.cs
+++ Semana 02/Tipo_Datos/obj/Debug/net8.0/Tipo_Datos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tipo_Datos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db5aec4c2c5b0787bcd3deb0400da6f9ccfaff8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99b0f9fe99fae13cab44e0f3f0d4bf301186f07b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tipo_Datos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tipo_Datos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
